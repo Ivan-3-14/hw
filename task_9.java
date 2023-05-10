7756ca0967fee0473dae715f0adb2b76c72f6b1a
Ivan_3_14_ <9129946@mail.ru>
2023-05-10 20:13:32 +0300

    lection commit

--- task_9.java
+++ task_9.java
@@ -16,134 +16,188 @@ public class task_9 {
 
         if (hours == 0) {
             if (minutes == 0) {
-                switch (secundes % 10) {
-                    case 0:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
+                switch (secundes % 100) {
+                    case 11:
+                    case 12:
+                    case 13:
+                    case 14:
                         System.out.print(" " + secundes + " секунд ");
                         break;
-                    case 2:
-                    case 3:
-                    case 4:
-                        System.out.print(" " + secundes + " секунды ");
-                        break;
-                    case 1:
-                        System.out.print(" " + secundes + " секунда ");
-                        break;
                     default:
-                        System.out.println();
+
+                        switch (secundes % 10) {
+                            case 0:
+                            case 5:
+                            case 6:
+                            case 7:
+                            case 8:
+                            case 9:
+                                System.out.print(" " + secundes + " секунд ");
+                                break;
+                            case 2:
+                            case 3:
+                            case 4:
+                                System.out.print(" " + secundes + " секунды ");
+                                break;
+                            case 1:
+                                System.out.print(" " + secundes + " секунда ");
+                                break;
+                            default:
+                                System.out.println();
+                        }
+
                 }
             } else {
-                switch (minutes % 10) {
-                    case 0:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        System.out.print(minutes + " минут ");
-                        break;
-                    case 2:
-                    case 3:
-                    case 4:
-                        System.out.print(minutes + " минуты ");
-                        break;
-                    case 1:
-                        System.out.print(minutes + " минута ");
+                switch (minutes % 100) {
+                    case 11:
+                    case 12:
+                    case 13:
+                    case 14:
+                        System.out.print(" " + minutes + " минут ");
                         break;
                     default:
-                        System.out.println();
+                        switch (minutes % 10) {
+                            case 0:
+                            case 5:
+                            case 6:
+                            case 7:
+                            case 8:
+                            case 9:
+                                System.out.print(minutes + " минут ");
+                                break;
+                            case 2:
+                            case 3:
+                            case 4:
+                                System.out.print(minutes + " минуты ");
+                                break;
+                            case 1:
+                                System.out.print(minutes + " минута ");
+                                break;
+                            default:
+                                System.out.println();
+                        }
                 }
-
-                switch (secundes % 10) {
-                    case 0:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
+                switch (secundes % 100) {
+                    case 11:
+                    case 12:
+                    case 13:
+                    case 14:
                         System.out.print(" " + secundes + " секунд ");
                         break;
-                    case 2:
-                    case 3:
-                    case 4:
-                        System.out.print(" " + secundes + " секунды ");
-                        break;
-                    case 1:
-                        System.out.print(" " + secundes + " секунда ");
-                        break;
                     default:
-                        System.out.println();
+                        switch (secundes % 10) {
+                            case 0:
+                            case 5:
+                            case 6:
+                            case 7:
+                            case 8:
+                            case 9:
+                                System.out.print(" " + secundes + " секунд ");
+                                break;
+                            case 2:
+                            case 3:
+                            case 4:
+                                System.out.print(" " + secundes + " секунды ");
+                                break;
+                            case 1:
+                                System.out.print(" " + secundes + " секунда ");
+                                break;
+                            default:
+                                System.out.println();
+                        }
                 }
 
 
             }
         } else {
-            switch (hours % 10) {
-                case 0:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
+            switch (hours % 100) {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
                     System.out.print(" " + hours + " часов ");
                     break;
-                case 2:
-                case 3:
-                case 4:
-                    System.out.print(" " + hours + " часa ");
-                    break;
-                case 1:
-                    System.out.print(" " + hours + " час ");
+                default:
+                    switch (hours % 10) {
+                        case 0:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                            System.out.print(" " + hours + " часов ");
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            System.out.print(" " + hours + " часa ");
+                            break;
+                        case 1:
+                            System.out.print(" " + hours + " час ");
+                            break;
+                        default:
+                            System.out.println("ошибка");
+                    }
+            }
+            switch (minutes % 100) {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    System.out.print(" " + minutes + " минут ");
                     break;
                 default:
-                    System.out.println("ошибка");
+                    switch (minutes % 10) {
+                        case 0:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                            System.out.print(minutes + " минут ");
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            System.out.print(minutes + " минуты ");
+                            break;
+                        case 1:
+                            System.out.print(minutes + " минута ");
+                            break;
+                        default:
+                            System.out.println();
+                    }
             }
-
-            switch (minutes % 10) {
-                case 0:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                    System.out.print(minutes + " минут ");
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    System.out.print(minutes + " минуты ");
-                    break;
-                case 1:
-                    System.out.print(minutes + " минута ");
-                    break;
-                default:
-                    System.out.println();
-            }
-            switch (secundes % 10) {
-                case 0:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
+            switch (secundes % 100) {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
                     System.out.print(" " + secundes + " секунд ");
                     break;
-                case 2:
-                case 3:
-                case 4:
-                    System.out.print(" " + secundes + " секунды ");
-                    break;
-                case 1:
-                    System.out.print(" " + secundes + " секунда ");
-                    break;
                 default:
-                    System.out.println();
+                    switch (secundes % 10) {
+                        case 0:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                            System.out.print(" " + secundes + " секунд ");
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            System.out.print(" " + secundes + " секунды ");
+                            break;
+                        case 1:
+                            System.out.print(" " + secundes + " секунда ");
+                            break;
+                        default:
+                            System.out.println();
 
 
+                    }
             }
         }
     }
