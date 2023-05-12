9a7697f41e1576498fdb662357ab3e5308703cb0
Ivan_3_14_ <9129946@mail.ru>
2023-05-12 22:32:39 +0300

    with homework1 commit

---
+++ homework1.java
@@ -0,0 +1,30 @@
+public class homework1 {
+    public static void main(String[] args) {
+        int sideA = 25;
+        int sideB = 19;
+        int sideC = 40;
+        int sideD = 31;
+        int sideE = 50;
+        int sideF = 40;
+
+        if (sideB + sideD <= sideF & sideA <= sideE & sideC <= sideE) {
+            System.out.println("дома помещаются");
+        } else if (sideB + sideD <= sideE & sideA <= sideF & sideC <= sideF) {
+            System.out.println("дома помещаются");
+        } else if (sideA + sideC <= sideF & sideB <= sideE & sideD <= sideE) {
+            System.out.println("дома помещаются");
+        } else if (sideA + sideC <= sideE & sideB <= sideF & sideD <= sideF) {
+            System.out.println("дома помещаются");
+        } else if (sideA + sideD <= sideF & sideB <= sideE & sideC <= sideE) {
+            System.out.println("дома помещаются");
+        } else if (sideA + sideD <= sideE & sideB <= sideF & sideC <= sideF) {
+            System.out.println("дома помещаются");
+        } else if (sideC + sideB <= sideF & sideA <= sideE & sideD <= sideE) {
+            System.out.println("дома помещаются");
+        } else if (sideC + sideB <= sideE & sideA <= sideF & sideD <= sideF) {
+            System.out.println("дома помещаются");
+        } else {
+            System.out.println("дома не помещаются");
+        }
+    }
+}
