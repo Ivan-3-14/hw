631da0545dc9cfdfbad21e1987e4033d735145e6
Ivan_3_14_ <9129946@mail.ru>
2023-06-29 11:17:22 +0300

    re_two_rn commit

--- homework/re_two_rn/Task1.java
+++ homework/re_two_rn/Task1.java
@@ -5,19 +5,23 @@ import java.util.regex.Pattern;
 public class Task1 {
     public static void main(String[] args) {
 
-        String stringFromTaskOne = "I make 5 mistakes in my homework 29days ago, and have more 100 reasons for it 27.06.2023";
-        String result;
+        String stringFromTaskOne = "I make 5 mistakes in my homework 29 days ago, and have more 100 reasons for it 29.06.2023";
         String longesNumber = null;
 
         var pattern = Pattern.compile("\\d*");
         var matcher = pattern.matcher(stringFromTaskOne);
 
         while (matcher.find()) {
-                int length = matcher.end() - matcher.start();
-                if (longesNumber == null || longesNumber.length() < length) {
-                    longesNumber = stringFromTaskOne.substring(matcher.start(), matcher.end());
-                }
+            int length = matcher.end() - matcher.start();
+            if (longesNumber == null || longesNumber.length() < length) {
+                longesNumber = stringFromTaskOne.substring(matcher.start(), matcher.end());
             }
-            result = longesNumber;
+        }
+
+        if (longesNumber.equals("")) {
+            System.out.println("This text haven't numbers");
+        } else {
+            System.out.println(longesNumber);
+        }
     }
 }
